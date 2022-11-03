5886d3290ff97c48d794ebbef4549365c50ae305
Anirudh Parida <86073238+AnirudhParida@users.noreply.github.com>
2022-11-04 03:23:06 +0530

    Login

--- app/src/main/java/com/example/innovate/MainActivity.kt
+++ app/src/main/java/com/example/innovate/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.innovate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.innovate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnLogin?.setOnClickListener {
+            if (binding?.etUserName?.text?.isEmpty() == true){
+                Toast.makeText(this, "Enter your User Name", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
