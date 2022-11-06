07baef10a3781e0e086882e39aaafa575716c325
Anirudh Parida <86073238+AnirudhParida@users.noreply.github.com>
2022-11-06 11:16:13 +0530

    activity_home_page ui refreshments and profile pic circle

--- app/src/main/java/com/example/innovate/MyProfile.kt
+++ app/src/main/java/com/example/innovate/MyProfile.kt
@@ -6,11 +6,15 @@ import com.example.innovate.databinding.ActivityHomePageBinding
 import com.example.innovate.databinding.ActivityMyProfileBinding
 
 class MyProfile : AppCompatActivity() {
+
     private var binding : ActivityMyProfileBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMyProfileBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
         val currentUser : userProfile? = userConstant.thisUser
         binding?.userName?.text=currentUser?.shortName
         currentUser?.userImage?.let { binding?.userImageId?.setImageResource(it) }
@@ -18,7 +22,6 @@ class MyProfile : AppCompatActivity() {
         binding?.noFollowers?.text=currentUser?.noFollowers.toString()
         binding?.noIdeas?.text=currentUser?.noIdeas.toString()
         binding?.noProjects?.text=currentUser?.noProjects.toString()
-
 
 
     }
